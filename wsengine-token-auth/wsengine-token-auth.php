7bfb1a3994d0f8cfb0929a81f994cd2bd5b298db
wsengine <167367157+wsengine@users.noreply.github.com>
2024-05-13 16:53:49 -0600

    Update wsengine-token-auth.php
    Redirect to admin panel if already logged in.

--- wsengine-token-auth/wsengine-token-auth.php
+++ wsengine-token-auth/wsengine-token-auth.php
@@ -10,9 +10,16 @@
 
 add_action('login_init', function()
 {
-	// do not continue if logged in or no token
-	if( is_user_logged_in() || !$_GET['auth_token'] )
+	// do not continue if no token
+	if( !array_key_exists('auth_token', $_GET) || !$_GET['auth_token'] )
 		return;
+
+	// redirect to admin panel if already logged in
+	if( is_user_logged_in() )
+	{
+		wp_safe_redirect('/wp-admin/');
+		exit;
+	}
 
 	// decode token
 	$token = base64_decode($_GET['auth_token']);
